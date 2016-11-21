6f0bf6d625a8565fd1a754b6e9d7c172864bcb0a
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-11-21 12:48:39 +0300

    small fixes

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ INSTALL_DIR="/etc/aktos/service-runner"
 
 echo "Installing in $INSTALL_DIR"
 sudo mkdir -p $INSTALL_DIR
-cp "tmux-launch" /tmp/tmux-launch 
-sudo cp /tmp/tmux-launch $INSTALL_DIR
+sudo cp "tmux-launch" $INSTALL_DIR
 echo "all done..."
