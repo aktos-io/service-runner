9e020637ae3b430d883f5d557c9b8d8c85d685df
cca <cem@aktos.io>
2016-12-18 20:34:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 INSTALL_DIR="/etc/aktos/service-runner"
-
+sudo apt-get install tmux 
 echo "Installing in $INSTALL_DIR"
 sudo mkdir -p $INSTALL_DIR
 sudo cp "tmux-launch" $INSTALL_DIR
