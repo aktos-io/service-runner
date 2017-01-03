2e7fd4d3eb5dc9701b5e87dda57af522df4b75f3
root <root@cca-erik>
2017-01-03 05:58:06 +0300

    improved install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-INSTALL_DIR="/etc/aktos/service-runner"
+INSTALL_DIR="/etc/aktos"
 sudo apt-get install tmux 
 echo "Installing in $INSTALL_DIR"
 sudo mkdir -p $INSTALL_DIR
-sudo cp "tmux-launch" $INSTALL_DIR
+sudo git clone . "$INSTALL_DIR/service-runner"
 echo "all done..."
